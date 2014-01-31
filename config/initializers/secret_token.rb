377f468de16f70b7ef49c572272365da9a84944d
Kyle S. Owens <kyleowens10@gmail.com>
2014-01-30 22:07:55 -0500

    Initial commit, base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coinplay::Application.config.secret_token = '1a915ed78d642e9921788f77e9cf301dd097f2f4103f491a191c2f326630fead3620b17c5721e9a368a894058b33ad910cf45cbc23a15aa776492c631ea8a922'
