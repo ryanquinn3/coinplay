3cb48c24d37c2e93777534f18b3e77619ba0e204
Kyle S. Owens <kyleowens10@gmail.com>
2014-01-30 23:43:01 -0500

    User_game_profile model and tests updated

--- app/models/user_game_profile.rb
+++ app/models/user_game_profile.rb
@@ -5,4 +5,32 @@ class UserGameProfile < ActiveRecord::Base
   belongs_to :games, :foreign_key => "game_id"
   belongs_to :users, :foreign_key => "user_id"
 
+  # Validations
+  # make sure required fields are present
+  validates_presence_of :game_id, :user_id, :username, :verified, :region
+
+  validates_numericality_of :game_id, :user_id, :region, :wins, :losses
+  validates_numericality_of :username_id, :allow_nil => true
+  validates_numericality_of :rank, :allow_nil => true
+  validates_numericality_of :league, :allow_nil => true
+
+  # WRITE TESTS FOR
+  #validate :valid_game_id
+  #validate :valid_user_id
+
+  private
+  def valid_game_id
+  	game_ids = Game.all.map{|g| g.id}
+	unless game_ids.include?(self.game_id)
+		errors.add(:game, "is not a valid game")
+	end
+  end
+
+  def valid_user_id
+  	user_ids = User.all.map{|u| u.id}
+	unless user_ids.include?(self.user_id)
+		errors.add(:user, "is not a valid user")
+	end
+  end
+
 end
--- test/unit/user_game_profile_test.rb
+++ test/unit/user_game_profile_test.rb
@@ -6,4 +6,31 @@ class UserGameProfileTest < ActiveSupport::TestCase
 	should belong_to(:users)
 	should belong_to(:games)
 
+	# Validation tests
+	should validate_presence_of(:game_id)
+	should validate_presence_of(:user_id)
+	should validate_presence_of(:username)
+	should validate_presence_of(:verified)
+	should validate_presence_of(:region)
+
+	should validate_numericality_of(:game_id)
+	should validate_numericality_of(:user_id)
+	should validate_numericality_of(:region)
+	should validate_numericality_of(:wins)
+	should validate_numericality_of(:losses)
+	should_not allow_value(nil).for(:game_id)
+	should_not allow_value(nil).for(:user_id)
+	should_not allow_value(nil).for(:region)
+	should_not allow_value(nil).for(:wins)
+	should_not allow_value(nil).for(:losses)
+
+	should validate_numericality_of(:username_id)
+	should validate_numericality_of(:rank)
+	should validate_numericality_of(:league)
+	should allow_value(nil).for(:username_id)
+	should allow_value(nil).for(:rank)
+	should allow_value(nil).for(:league)
+
+	# context test
+
 end
