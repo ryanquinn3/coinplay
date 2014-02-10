813b5bb607364d77f163240c360643ebf36cd8b3
Kyle S. Owens <kyleowens10@gmail.com>
2014-02-10 17:12:32 -0500

    Spacing edited

--- db/schema.rb
+++ db/schema.rb
@@ -39,10 +39,10 @@ ActiveRecord::Schema.define(:version => 20140131044523) do
     t.string   "username"
     t.string   "password_digest"
     t.float    "bitcoin_balance", :default => 0.0
-    t.string   "type",            :default => "player"
+    t.string   "type"
     t.boolean  "active",          :default => true
-    t.datetime "created_at",                            :null => false
-    t.datetime "updated_at",                            :null => false
+    t.datetime "created_at",                        :null => false
+    t.datetime "updated_at",                        :null => false
   end
 
 end
