4071956c51552031bf33bb82b84bc6db30e4ef4f
Kyle S. Owens <kyleowens10@gmail.com>
2014-02-10 19:11:04 -0500

    Lobby validations and tests added

--- app/models/lobby.rb
+++ app/models/lobby.rb
@@ -6,4 +6,12 @@ class Lobby < ActiveRecord::Base
   belongs_to :maps, :foreign_key => "map_id"
   belongs_to :game_types, :foreign_key => "game_type_id"
 
+  # Validations
+  validates_presence_of :team_one_id, :team_two_id, :game_id, :map_id, :game_type_id
+  validates_presence_of :team_size, :wager_amount
+  validates_numericality_of :team_one_id, :team_two_id, :game_id, :map_id, :game_type_id
+  validates_numericality_of :wager_amount, :greater_than_or_equal_to => 0.001, :less_than_or_equal_to => 1
+
+  # NEED MORE VALIDATIONS?
+
 end
--- test/unit/lobby_test.rb
+++ test/unit/lobby_test.rb
@@ -7,4 +7,35 @@ class LobbyTest < ActiveSupport::TestCase
 	should belong_to(:game_types)
 	should belong_to(:games)
 
+	# Validation tests
+	should validate_presence_of(:team_one_id)
+	should validate_presence_of(:team_two_id)
+	should validate_presence_of(:game_id)
+	should validate_presence_of(:map_id)
+	should validate_presence_of(:game_type_id)
+
+	should validate_presence_of(:team_size)
+	should validate_presence_of(:wager_amount)
+
+	should validate_numericality_of(:team_one_id)
+	should validate_numericality_of(:team_two_id)
+	should validate_numericality_of(:game_id)
+	should validate_numericality_of(:map_id)
+	should validate_numericality_of(:game_type_id)
+	should_not allow_value(nil).for(:team_one_id)
+	should_not allow_value(nil).for(:team_two_id)
+	should_not allow_value(nil).for(:game_id)
+	should_not allow_value(nil).for(:map_id)
+	should_not allow_value(nil).for(:game_type_id)
+
+	should validate_numericality_of(:wager_amount)
+	should allow_value(0.001).for(:wager_amount)
+	should allow_value(1).for(:wager_amount)
+	should allow_value(0.5).for(:wager_amount)
+	should allow_value(0.005).for(:wager_amount)
+	should_not allow_value(nil).for(:wager_amount)
+	should_not allow_value(1.001).for(:wager_amount)
+	should_not allow_value(5).for(:wager_amount)
+	should_not allow_value(0.0001).for(:wager_amount)
+
 end
